44ea514d355de81d77bd79b5b971f6d9ecee2ab3
Ali Zayed <aliahmedzayed2000@gmail.com>
2024-09-30 14:31:50 +0300

    Refined Implementations

--- src/main/java/org/polygon/engine/core/Window.java
+++ src/main/java/org/polygon/engine/core/Window.java
@@ -1,2 +1,180 @@
-package org.polygon.engine.core;public class Window {
+package org.polygon.engine.core;
+
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+import java.util.concurrent.Callable;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL40.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class Window {
+
+    // The window handle
+    private long windowHandle;
+    private int width;
+    private int height;
+    private Callable<Void> resizeFunc;
+
+    public Window(String title, WindowOptions opts, Callable<Void> resizeFunc) {
+        this.resizeFunc = resizeFunc;
+
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if ( !glfwInit() )
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Configure GLFW
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+        // Sets glfw OpenGL context version to 4.x
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+        // Checks if compatible profile is enabled, (default: false)
+        if(opts.compatibleProfile) {
+            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+        } else {
+            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+            glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+        }
+
+        // Get the resolution of the primary monitor
+        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+        if(opts.width > 0 && opts.height > 0) {
+            this.width = opts.width;
+            this.height = opts.height;
+        } else {
+            glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+            this.width = vidmode.width();
+            this.height = vidmode.height();
+        }
+
+        // Create the window
+        windowHandle = glfwCreateWindow(width, height, title, NULL, NULL);
+        if ( windowHandle == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        // Setup a window resize callback. It will be called every time the window is resized
+        glfwSetFramebufferSizeCallback(windowHandle, (window, w, h) -> resized(w, h));
+
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(windowHandle, (window, key, scancode, action, mods) -> {
+            keyCallBack(key, action);
+        });
+
+        // Get the thread stack and push a new frame
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(windowHandle, pWidth, pHeight);
+
+            this.width = pWidth.get(0);
+            this.height =pHeight.get(0);
+
+            if(!(glfwGetWindowAttrib(windowHandle, GLFW_MAXIMIZED) == GLFW_TRUE)) {
+                // Center the window
+                glfwSetWindowPos(
+                        windowHandle,
+                        (vidmode.width() - pWidth.get(0)) / 2,
+                        (vidmode.height() - pHeight.get(0)) / 2
+                );
+            }
+        } // the stack frame is popped automatically
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(windowHandle);
+
+        // If a custom fps is set, it will disable v-sync
+        if(opts.fps > 0) {
+            // disable v-sync
+            glfwSwapInterval(0);
+        } else {
+            // enable v-sync
+            glfwSwapInterval(1);
+        }
+
+        // Make the window visible
+        glfwShowWindow(windowHandle);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public long getWindowHandle() {
+        return windowHandle;
+    }
+
+    public boolean isKeyPressed(int keyCode) {
+        return glfwGetKey(windowHandle, keyCode) == GLFW_PRESS;
+    }
+
+    public void pollEvents() {
+        // Poll for window events. The key callback above will only be
+        // invoked during this call.
+        glfwPollEvents();
+    }
+
+    public void update() {
+        glfwSwapBuffers(windowHandle); // swap the color buffers
+    }
+
+    public boolean windowShouldClose() {
+        return glfwWindowShouldClose(windowHandle);
+    }
+
+    public void cleanup() {
+        // Free the window callbacks and destroy the window
+        glfwFreeCallbacks(windowHandle);
+        glfwDestroyWindow(windowHandle);
+
+        // Terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    private void keyCallBack(int key, int action) {
+        // If Escape key is pressed, the window will close;
+        if(key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+            glfwSetWindowShouldClose(windowHandle, true);
+        }
+    }
+
+    private void resized(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        try {
+            resizeFunc.call();
+        } catch(Exception exception) {
+            System.err.println("Error calling the resize callback function\n" + exception.getMessage());
+        }
+    }
+
+    public static class WindowOptions {
+        public boolean compatibleProfile;
+        public int fps;
+        public int ups = Engine.TARGET_UPS;
+        public int width;
+        public int height;
+    }
 }
--- src/main/java/org/polygon/game/Main.java
+++ src/main/java/org/polygon/game/Main.java
@@ -1,6 +1,15 @@
-package org.polygon.engine.core;
+package org.polygon.game;
+
+import org.polygon.engine.core.Engine;
+import org.polygon.engine.core.Window;
 
 public class Main {
     public static void main(String[] args) {
+        GameScript script = new GameScript();
+        Window.WindowOptions options = new Window.WindowOptions();
+        options.width = 1920;
+        options.height = 1080;
+        Engine engine = new Engine("Polygon Game Engine", options, script);
+        engine.start();
     }
 }
