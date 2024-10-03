8896fe41eafc9a05f51f39ad9c7778e67258e6a2
Ali Zayed <aliahmedzayed2000@gmail.com>
2024-10-03 13:46:27 +0300

    Implementation for ShaderProgram entity
     -Implemented ShaderProgram entity that will compile and hold shader program reference used in rendering. -Added Utils module to help reading files into strings. -Basic implementation for scene's vertex and fragment shaders.

---
+++ src/main/java/org/polygon/engine/core/Utils.java
@@ -0,0 +1,23 @@
+package org.polygon.engine.core;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Utils {
+
+    private Utils() {
+
+    }
+
+    // reads file and saves it into a string object
+    public static String readFiles(String filePath) {
+        String str;
+        try {
+            str = new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch(IOException exception) {
+            throw new RuntimeException("Couldn't read file [" + filePath + "] ", exception);
+        }
+        return str;
+    }
+}
